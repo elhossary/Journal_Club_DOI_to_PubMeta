a97a3b9d4975ab5e89ea9f0a39eb81b7e9eed8cc
Muhammad Elhossary <muhammad_elhossary@hotmail.com>
2018-11-27 11:05:49 +0100

    Update meta_doi.py

--- meta_doi.py
+++ meta_doi.py
@@ -6,17 +6,22 @@ import sys, getopt
 
 def hard_article_search(doi):
   url = "https://www.ncbi.nlm.nih.gov/pubmed/?term=" + doi
-  pmid = ""
-  str_received = url_request.urlopen(url).read().decode('utf-8')
+  try:
+    str_received = url_request.urlopen(url).read().decode('utf-8')
+  except Exception as e:
+    print(e)
+    print("DOI search skipped due to network issue")
+    return None
+  if "The following term was not found in PubMed" in str_received:
+    return None
   if "<dt>PMID:</dt>" in str_received:
-    result = re.search('<dl class="rprtid"><dt>PMID:</dt> <dd>(.*)</dd>', str_received).group(1)
-    try:
-      pmid = result[:result.index('<')]
-      return pmid
-    except:
-      print("A DOI found in Pubmed but seems there is problem with this DOI, Please do it manually ==> " + doi)
-    finally:
-      return pmid
+    result = re.search(r'<dt>PMID:</dt> <dd>(.*)</dd>', str_received).group(0)
+    result = re.search(r'<dt>PMID:</dt> <dd>(.*)</dd>', result).group(1)
+    result = re.sub(r"</dd>(.*)", "", result)
+    if result.isdigit():
+      return result
+    else:
+      return None
   else:
     return None
 
@@ -28,18 +33,17 @@ def convert_doi_to_pmid(doi_csv):
     print(e)
     sys.exit("Excution Aborted, service unavailable")
   parsed_xml = ET.fromstring(str_received)
-  dfcols = ['doi', 'pmid']
+  dfcols = ['DOI', 'PMID']
   doi_pmid_df = pd.DataFrame(columns=dfcols)
+
   for child in parsed_xml.iter(tag="record"):
-    doi_pmid_df = doi_pmid_df.append(
-      pd.Series([child.get('requested-id'), child.get('pmid')], index=dfcols), ignore_index=True)
+    doi_pmid_df = doi_pmid_df.append(pd.Series([child.get('requested-id'),
+                                                child.get('pmid')], index=dfcols), ignore_index=True)
     doi_pmid_df.head()
 
   for index, row in doi_pmid_df.iterrows():
-    if not row['pmid']:
-      row['pmid'] = hard_article_search(row['doi'])
-      if not row['pmid']:
-        print("Can't find this DOI in PubMed ==> " + row['doi'])
+    if not row['PMID']:
+      row['PMID'] = hard_article_search(row['DOI'])
   return doi_pmid_df
 
 def getvalueofnode(article, search_tag, val_mode):
@@ -50,6 +54,7 @@ def getvalueofnode(article, search_tag, val_mode):
     for node in article.iter():
       if node.tag == search_tag:
         return_val =  node.text
+        break
   elif val_mode == "MULTI":
     for node in article.iter():
       if node.tag == search_tag:
@@ -58,6 +63,7 @@ def getvalueofnode(article, search_tag, val_mode):
             authors_list.append(subnode.text)
           elif subnode.tag == "ForeName":
             authors_list[-1] += (" " + subnode.text)
+        break
     for i in authors_list:
       authors += i + ", "
     return_val = authors[:-2]
@@ -101,7 +107,7 @@ def generate_pub_list(pmid_csv):
     print(e)
     sys.exit("Excution Aborted, service unavailable")
   article_set = ET.fromstring(str_received)
-  dfcols = ['Title', 'Authors', 'Abstract', 'Publisher', 'PubYear', 'PubMonth', 'PMID', 'DOI']
+  dfcols = ['Title', 'Authors', 'Abstract', 'Publisher', 'PubYear', 'PubMonth', 'PMID']
   df = pd.DataFrame(columns=dfcols)
   for article in article_set:
     df = df.append(pd.Series([getvalueofnode(article, "ArticleTitle", "SINGLE"),
@@ -110,8 +116,7 @@ def generate_pub_list(pmid_csv):
                               getvalueofnode(article, "Title", "SINGLE"),
                               getvalueofnode(article, "Year", "SINGLE"),
                               getvalueofnode(article, "Month", "SINGLE"),
-                              getvalueofnode(article, "PMID", "SINGLE"),
-                              None],
+                              getvalueofnode(article, "PMID", "SINGLE")],
                               index=dfcols), ignore_index=True)
   # for doi in cross_ref_doi_list:
   #   df = df.append(search_in_cross_ref(doi), ignore_index=True)
@@ -134,48 +139,52 @@ def main(argv):
       inputfilename = arg
     elif opt in ("-o", "--ofile"):
       outputfilename = arg
+
   inputfile = open(inputfilename, "r", encoding="utf-8")
   doi_pmid_df = convert_doi_to_pmid(inputfile.read())
   inputfile.close()
   pmid_csv = ""
 
   for index, row in doi_pmid_df.iterrows():
-    if row['pmid']:
-      pmid_csv += row['pmid'] + ","
+    if row['PMID']:
+      pmid_csv += row['PMID'] + ","
     else:
-      error_doi_list.append(row['doi'])
-    #   pmid_csv += "CROSS_REF_SEARCH_" + row['doi'] +"_THIS,"
+      error_doi_list.append(row['DOI'])
+    #   pmid_csv += "CROSS_REF_SEARCH_" + row['DOI'] +"_THIS,"
+  doi_pmid_df = doi_pmid_df.dropna()
   pmid_csv = pmid_csv[:-1]
-  df = generate_pub_list(pmid_csv)
-  for index, row in df.iterrows():
-    for i, r in doi_pmid_df.iterrows():
-      if row["PMID"] == r["pmid"]:
-        row["DOI"] = r["doi"]
-        break
+  df_pub_generated = generate_pub_list(pmid_csv)
+  df_pub_generated = pd.merge(df_pub_generated, doi_pmid_df, on="PMID", how='inner')
+
   md_code_str = "# Journal Club Publication List\n---\n"
-  for index, row in df.iterrows():
-    md_code_str += "> ### " + row['Title'] + "\n"
-    md_code_str += "> ###### " + row['Publisher'] + ", Date: " + row['PubMonth'] + "/" + row['PubYear'] + "\n\n"
-    md_code_str += "> #### " + row['Authors'] + "\n"
-    md_code_str += "> ##### " + row['Abstract'] + "\n\n"
-    if row['PMID'] and row['DOI']:
-      md_code_str += "> ##### Link to article: [Pubmed](https://www.ncbi.nlm.nih.gov/pubmed/"\
-                     + row['PMID'] + "), [DOI](http://doi.org/" + row['DOI'] + ")\n\n---\n"
-    elif row['DOI'] and not row['PMID']:
-      md_code_str += "> ##### Link to article: [DOI](http://doi.org/" + row['DOI'] + ")\n\n---\n"
-    elif row['PMID'] and not row['DOI']:
-      md_code_str += "> ##### Link to article: [Pubmed](https://www.ncbi.nlm.nih.gov/pubmed/" + row['PMID'] + ")\n\n---\n"
-  outputfile = open(outputfilename + ".md", "w", encoding="utf-8")
+  counter = 0
+  for index, row in df_pub_generated.iterrows():
+    if row['DOI']:
+      counter += 1
+      md_code_str += "> ### " + str(counter) + "- " + row['Title'] + "\n"
+      md_code_str += "> ###### " + row['Publisher'] + ", Date: " + row['PubMonth'] + "/" + row['PubYear'] + "\n\n"
+      md_code_str += "> #### " + row['Authors'] + "\n"
+      md_code_str += "> ##### " + row['Abstract'] + "\n\n"
+      if row['PMID']:
+        md_code_str += "> ##### Link to article: [Pubmed](https://www.ncbi.nlm.nih.gov/pubmed/" \
+                       + row['PMID'] + "), [DOI](http://doi.org/" + row['DOI'] + ")\n\n---\n"
+    else:
+      print("problem here")
+  outputfile = open(outputfilename + ".md", "w+", encoding="utf-8")
   outputfile.write(md_code_str)
-  outputfile.close()
+  outputfile.write("> DOIs to be checked manually:\n")
+
   if len(error_doi_list) != 0:
     error_file = open("DOI_errors.txt", "w+", encoding="utf-8")
     error_file.write("DOIs that has errors for manual check:")
     for err_doi in error_doi_list:
+      counter += 1
       error_file.write("\n" + err_doi)
+      outputfile.write("> ##### " + str(counter) + "- " + "http://doi.org/" + err_doi + "\n")
     error_file.close()
-    print('\n\n\n\n***** DONE with ' + str(len(error_doi_list)) + ' errors of ' + str(doi_pmid_df.shape[0])
-          + ' , check file "DOI_errors.txt" *****')
+    outputfile.close()
+    print('\n\n\n\n***** DONE with ' + str(len(error_doi_list)) + ' errors, ' + str(doi_pmid_df.shape[0])
+          + ' success, check file "DOI_errors.txt" *****')
   else:
     print("\n\n\n\n***** DONE without errors! *****")
 
